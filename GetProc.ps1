889a8ff0fecff4f9abe04098b6175faa8fe4e1d8
BuildTools <unconfigured@null.spigotmc.org>
2024-07-29 20:19:09 +0200

    Changed to Typed output

--- GetProc.ps1
+++ GetProc.ps1
@@ -5,18 +5,31 @@
 #######################################################################
 
 
+# ProcName: Process name to monitor
+# Type:
+#   1: CPU
+#   2: MEM
 param (
-    [string]$ProcName
+    [string]$ProcName,
+    [int]$Type
 )
 
 $Proc = Get-Process -Name $ProcName -ErrorAction SilentlyContinue
 
 if ($null -ne $Proc) {
-    $CPU = (Get-WmiObject Win32_PerfFormattedData_PerfProc_Process -Filter "IDProcess=$($Proc.Id)").PercentProcessorTime
-    $Mem = [math]::round($Proc.WorkingSet64 / 1KB, 2)
-    Write-Output "$ProcName, $CPU, $Mem"
+    $Out = "null"
+    if( $Type -eq 1)
+    {
+        $Out = (Get-WmiObject Win32_PerfFormattedData_PerfProc_Process -Filter "IDProcess=$($Proc.Id)").PercentProcessorTime
+    }
+    elseif( $Type -eq 2)
+    {
+        $Out = [math]::round($Proc.WorkingSet64 / 1KB, 2)
+    }
+    
+    Write-Output "$Out"
 } else {
-    Write-Output "null, 0, 0"
+    Write-Output "null"
 }
 
 
